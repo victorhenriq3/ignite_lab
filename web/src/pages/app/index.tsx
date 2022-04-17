997a5fbbffcbd4802ec0b7a2d13d6b2d6dfc0627
victorhenriqc <vic.1601@outlook.com>
2022-04-17 12:04:57 -0300

    first commit

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable @next/next/no-html-link-for-pages */
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
+
+
+export default function Home(){
+    const {user} = useUser()
+
+    return (
+        <div>
+            <h1>Hello world</h1>
+
+            <pre>
+                {JSON.stringify(user, null, 2)}
+            </pre>
+
+            <a href="/api/auth/logout">Logout</a>
+        </div>
+    )
+}
+
+export const getServerSideProps = withPageAuthRequired();
