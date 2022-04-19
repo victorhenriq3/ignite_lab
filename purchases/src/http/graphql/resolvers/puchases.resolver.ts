48c2f3cf38491ac7ab7f3142b8119a7bcd59ec86
victorhenriqc <vic.1601@outlook.com>
2022-04-18 22:36:56 -0300

    get the user is from auth 0

--- purchases/src/http/graphql/resolvers/puchases.resolver.ts
+++ purchases/src/http/graphql/resolvers/puchases.resolver.ts
@@ -1,8 +1,17 @@
 import { UseGuards } from '@nestjs/common';
-import { Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
 import { AuthorizationGuard } from 'src/http/auth/authorization.guard';
+import { AuthUser, CurrentUser } from 'src/http/auth/currentUser';
 import { ProductsService } from 'src/services/products.service';
 import { PurchaseServices } from 'src/services/purchase.service';
+import { CreatePurchaseInput } from '../inputs/create-purchase-input';
 import { Product } from '../models/product';
 import { Purchase } from '../models/purchase';
 
@@ -22,5 +31,14 @@ export class PurchaseResolver {
   @ResolveField(() => Product)
   product(@Parent() purchase: Purchase) {
     return this.productService.getProductById(purchase.productId);
+  }
+
+  @Mutation(() => Purchase)
+  @UseGuards(AuthorizationGuard)
+  createPurchase(
+    @Args('data') data: CreatePurchaseInput,
+    @CurrentUser() user: AuthUser,
+  ) {
+    return null;
   }
 }
--- purchases/src/services/purchase.service.ts
+++ purchases/src/services/purchase.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 
+interface CreatePurchaseParams {
+  productId: string;
+  customerId: string;
+}
 @Injectable()
 export class PurchaseServices {
   constructor(private prisma: PrismaService) {}
@@ -9,6 +13,25 @@ export class PurchaseServices {
     return await this.prisma.purchase.findMany({
       orderBy: {
         createdAt: 'desc',
+      },
+    });
+  }
+
+  async createPurchase({ productId, customerId }: CreatePurchaseParams) {
+    const product = await this.prisma.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      throw new Error('Product not found');
+    }
+
+    return await this.prisma.purchase.create({
+      data: {
+        customerId,
+        productId,
       },
     });
   }
