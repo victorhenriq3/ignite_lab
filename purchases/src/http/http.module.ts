033e2a197ae69a8a16ec9c9224018e2c46041faa
victorhenriqc <vic.1601@outlook.com>
2022-04-23 23:39:39 -0300

    config initial kafka

--- purchases/src/http/http.module.ts
+++ purchases/src/http/http.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import path from 'node:path';
 import { DatabaseModule } from 'src/database/database.module';
+import { MessagingModule } from 'src/messaging/messaging.module';
 import { CustomerService } from 'src/services/custumers.service';
 import { ProductsService } from 'src/services/products.service';
 import { PurchaseServices } from 'src/services/purchase.service';
@@ -15,6 +16,7 @@ import { PurchaseResolver } from './graphql/resolvers/puchases.resolver';
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    MessagingModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
--- purchases/src/services/purchase.service.ts
+++ purchases/src/services/purchase.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { KafkaService } from 'src/messaging/kafka.service';
 
 interface CreatePurchaseParams {
   productId: string;
@@ -7,7 +8,7 @@ interface CreatePurchaseParams {
 }
 @Injectable()
 export class PurchaseServices {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private kafka: KafkaService) {}
 
   async listAllPurchases() {
     return await this.prisma.purchase.findMany({
@@ -39,11 +40,28 @@ export class PurchaseServices {
       throw new Error('Product not found');
     }
 
-    return await this.prisma.purchase.create({
+    const purchase = await this.prisma.purchase.create({
       data: {
         customerId,
         productId,
       },
     });
+
+    const customer = await this.prisma.customer.findUnique({
+      where: { id: customerId },
+    });
+
+    this.kafka.emit('purchases.new-purchase', {
+      customer: {
+        authUserId: customer.authUserId,
+      },
+      product: {
+        id: product.id,
+        title: product.title,
+        slug: product.slug,
+      },
+    });
+
+    return purchase;
   }
 }
